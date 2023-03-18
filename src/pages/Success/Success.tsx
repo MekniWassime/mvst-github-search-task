ad6730211bf566ca0d5451f394aa7ed59bb10c7c
Mekni_Wassime <60438665+MekniWassime@users.noreply.github.com>
2023-03-18 22:46:07 +0100

    added try catch block to getAccessToken

--- src/pages/Success/Success.tsx
+++ src/pages/Success/Success.tsx
@@ -5,10 +5,16 @@ function Success() {
     const [searchParams] = useSearchParams();
 
     const getAccessToken = async (code: String)=>{
-        const response = await fetch(`https://mvst-task-proxy-service.onrender.com/getAccessToken?code=${code}`)
-        const data = await response.json()
-        localStorage.setItem("accessToken", data.access_token)
-        console.log(localStorage.getItem("accessToken"))
+        try {
+            console.log("getting access token from proxy")
+            const response = await fetch(`https://mvst-task-proxy-service.onrender.com/getAccessToken?code=${code}`)
+            const data = await response.json()
+            console.log(data)
+            localStorage.setItem("accessToken", data.access_token)
+            console.log(localStorage.getItem("accessToken"))
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect( () => {
