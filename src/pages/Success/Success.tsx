330424c9fac265315bae405cab779ba829acf3a6
Mekni_Wassime <60438665+MekniWassime@users.noreply.github.com>
2023-03-18 22:34:36 +0100

    saving the github oauth token in localstorage

--- src/pages/Success/Success.tsx
+++ src/pages/Success/Success.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom';
 
 function Success() {
     const [searchParams] = useSearchParams();
 
-    const code = searchParams.get("code");
+    const getAccessToken = async (code: String)=>{
+        const response = await fetch(`https://mvst-task-proxy-service.onrender.com/getAccessToken?code=${code}`)
+        const data = await response.json()
+        localStorage.setItem("accessToken", data.access_token)
+        console.log(localStorage.getItem("accessToken"))
+    }
+
+    useEffect( () => {
+        const code = searchParams.get("code");
+        if(code && localStorage.getItem("accessToken") === null){
+            getAccessToken(code);
+        }
+    }, [])
+    
+
 
   return (
     <div>LogIn in progress, Please wait</div>
