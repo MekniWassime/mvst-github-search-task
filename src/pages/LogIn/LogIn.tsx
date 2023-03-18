0d4582c4bcd08fd2b4a1aa9da3175e27dbd5444e
Mekni_Wassime <60438665+MekniWassime@users.noreply.github.com>
2023-03-18 23:46:54 +0100

    added Documentation to Login Page

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+/**
+ * LogIn page, redirects to Github's oauth page if user whiches to authenticate
+ */
 function LogIn() {
-const handleClick = () => {
-    window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
-}
+    /**
+     * used by Github oauth to determine which Github application the user wants to authorize
+     * in this case our application
+    */
+    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+    /**
+     * Take the user to Github's oauth page to authorize our Github Application
+     */
+    const handleClick = () => {
+        window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
+    }
 
   return (
     <>
